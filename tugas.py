d35945733a7c52f3c3985dce02639aae20a77f6d
robertheo <robertheo.rt@gmail.com>
2020-09-17 14:53:21 +0700

    inputFunction

--- tugas.py
+++ tugas.py
@@ -1,3 +1,15 @@
+import numpy as np
+
+
+def inputFunction():
+    arr = np.array([])
+    conf = 'Y'
+    while (conf == 'Y' or conf == 'y'):
+        inp = input("Masukan Input : ")
+        arr = np.append(arr, inp)
+        conf = input("continue? Y/N : ")
+
+
 print('Menu Pengurutan Bilangan')
 print('1.Masukkan Bilangan')
 print('2.Merubah Isi Array')
@@ -6,3 +18,6 @@ print('4.Menghapus Isi Array')
 print('5.Tampilkan Isi Array')
 print('6.Keluar')
 pilihan = input("Pilih Menu (1, 2, 3, 4, 5, 6) ? ")
+
+if pilihan == 1:
+    inputFunction()
