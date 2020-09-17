90eb6bb3150ed2c6745188e1baeffecb6a72ccd6
kennethliem <kennethliem991@gmail.com>
2020-09-17 11:10:42 +0700

    Update tugas.py
    Ken punya

--- tugas.py
+++ tugas.py
@@ -1,1 +1,10 @@
-print('hallo saya kenneth')
+# Happy Coding -- Ken
+import numpy as np
+
+def inputfunction():
+    arr = np.array([])
+    conf = 'Y'
+    while (conf == 'Y' or conf == 'y'):
+        inp = input("Masukan Input : ")
+        arr = np.append(arr,inp)
+        conf = input ("continue? Y/N : ")
