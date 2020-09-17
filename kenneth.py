5da4b4db5b590e92f276399658ad34dc8d6f56c2
kennethliem <kennethliem991@gmail.com>
2020-09-17 15:36:46 +0700

    Update kenneth.py

--- kenneth.py
+++ kenneth.py
@@ -3,8 +3,12 @@ import numpy as np
 
 def inputfunction():
     arr = np.array([])
-    conf = 'Y'
+    x = 0
+    x = input("Banyak Data Yang Ingin Di Input : ")
+
     while (conf == 'Y' or conf == 'y'):
         inp = input("Masukan Input : ")
         arr = np.append(arr,inp)
         conf = input ("continue? Y/N : ")
+
+inputfunction()
