a69c55fab7d48d99d15782bc09db25d16de4f0d2
patlayz24 <patricklay24@gmail.com>
2020-09-18 17:59:28 +0700

    Update patrick.py

--- patrick.py
+++ patrick.py
@@ -1,3 +1,13 @@
-print('hello world')
+import numpy as np
 
+def editFunction(): 
 
+    arr = np.array([12, 23, 34, 45, 56])
+    x = 0
+    x = int(input("indeks : "))
+    print(arr[x])
+
+    arr[x] = input("masukkan data baru : ")
+    print(arr)
+
+editFunction()
